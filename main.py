5bef3b8a6e37662c6b34b9307609772bf90f765a
Meltem DOĞAN <meltem2346@hotmail.com>
2024-05-05 00:10:04 +0300

    Cyberstart01_min_oklit

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+
+points = [(1, 2), (3, 4), (5, 6), (7, 8)]
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+distances = []
+min_distance = float('inf')
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+        if distance < min_distance:
+            min_distance = distance
+print("Tüm mesafeler:", distances)
+print("Minimum mesafe:", min_distance)
